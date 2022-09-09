3bd43c40260ddbe77c05a44ebe2f9f15dfa0ab4b
SUFandom <70519093+SUFandom@users.noreply.github.com>
2022-09-09 02:20:57 +0000

    12

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ Installing debdroid is quite easy, copy this one line code
 chmod +x *.sh ; ./bootstrap.sh
 ```
 
-After that, clone this work : `https://github.com/SUFandom/debdroid-starter`
+After that, clone this work
 
 But if theres bugs, head to [issues](http://github.com/SUFandom/debdroid/issues) page
 
--- assets/debian11-arm32.sh
+++ assets/debian11-arm32.sh
@@ -2,12 +2,12 @@
 
 
 DISTRO_NAME="Debian ARMv7"
-DISTRO_COMMENT="32 Bit version of Modified Debian with XFCE by yours truly, SUFandom (works with ARM64 too)"
+DISTRO_COMMENT="32 Bit version of Modified Debian Stable (works with ARM64 too)"
 
-TARBALL_URL['aarch64']="https://github.com/SUFandom/debdroid/releases/download/d-1.1/debian-arm32.tar.gz"
-TARBALL_SHA256['aarch64']="6e38eb32ccfa8e0229bf78705ae4c5a8cd8df2a646f2853f7931dfa2706e277a"
-TARBALL_URL['arm']="https://github.com/SUFandom/debdroid/releases/download/d-1.1/debian-arm32.tar.gz"
-TARBALL_SHA256['arm']="6e38eb32ccfa8e0229bf78705ae4c5a8cd8df2a646f2853f7931dfa2706e277a"
+TARBALL_URL['aarch64']="https://github.com/SUFandom/debdroid/releases/download/d-1.2/debian-arm32.tar.xz"
+TARBALL_SHA256['aarch64']="119706c9873d08413e6c7eb100c0aa4e3f5e1d37daa465beb2315ec959d5a6ad"
+TARBALL_URL['arm']="https://github.com/SUFandom/debdroid/releases/download/d-1.1/debian-arm32.tar.xz"
+TARBALL_SHA256['arm']="119706c9873d08413e6c7eb100c0aa4e3f5e1d37daa465beb2315ec959d5a6ad"
 
 
 distro_setup () {
