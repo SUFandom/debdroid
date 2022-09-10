42a72791e3292ae188515ab75c8b4b0ba03726c8
bluepearl394@gmail.com <bluepearl394@gmail.com>
2022-09-11 07:23:45 +0800

    push

--- assets/debian11-arm32.sh
+++ assets/debian11-arm32.sh
@@ -4,10 +4,10 @@
 DISTRO_NAME="Debian ARMv7"
 DISTRO_COMMENT="32 Bit version of Modified Debian Stable (works with ARM64 too)"
 
-TARBALL_URL['aarch64']="https://github.com/SUFandom/debdroid/releases/download/d-1.2/debian-arm32.tar.xz"
-TARBALL_SHA256['aarch64']="119706c9873d08413e6c7eb100c0aa4e3f5e1d37daa465beb2315ec959d5a6ad"
-TARBALL_URL['arm']="https://github.com/SUFandom/debdroid/releases/download/d-1.1/debian-arm32.tar.xz"
-TARBALL_SHA256['arm']="119706c9873d08413e6c7eb100c0aa4e3f5e1d37daa465beb2315ec959d5a6ad"
+TARBALL_URL['aarch64']="https://github.com/SUFandom/debdroid/releases/download/d-1.3/debian-arm32.tar.xz"
+TARBALL_SHA256['aarch64']="5c56bc36222719e3b57e854710aeddb346f7bb62065231f80f978a8c8ed956b6"
+TARBALL_URL['arm']="https://github.com/SUFandom/debdroid/releases/download/d-1.3/debian-arm32.tar.xz"
+TARBALL_SHA256['arm']="5c56bc36222719e3b57e854710aeddb346f7bb62065231f80f978a8c8ed956b6"
 
 
 distro_setup () {
