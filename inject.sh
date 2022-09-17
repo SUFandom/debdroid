6dfaa372b88dc7f4ba174770ed9d1f0d78d36ec1
SUFandom <70519093+SUFandom@users.noreply.github.com>
2022-09-17 12:55:33 +0000

    update inject.sh - Bugfix

--- inject.sh
+++ inject.sh
@@ -2,10 +2,16 @@
 
 if [ "$1" == "-arm32"  ]
 then
-rm -rf $PREFIX/var/lib/proot-distro/dlcache/*
 cp -r assets/debian11-arm32.sh $PREFIX/etc/proot-distro
 proot-distro install debian11-arm32
-proot-distro login debian11-arm32
+proot-distro login debian11-arm32 --/usr/bin/apt update
+proot-distro login debian11-arm32 --/usr/bin/apt upgrade -y
+proot-distro login debian11-arm32 --/usr/bin/apt install git -y
+proot-distro login debian11-arm32 --/usr/bin/git clone https://github.com/SUFandom/debdroid-starter
+clear
+echo "Please Start debdroid-starter to continue setup at /root/debdroid-starter, dont forget to chmod +x .sh files to work."
+sleep 10
+proot-distro login debian11-arm32 
 exit 0
 fi 
 
