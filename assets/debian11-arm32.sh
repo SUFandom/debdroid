5606df23858144edda1e4c62ce7de4114ffa421a
SUFandom <70519093+SUFandom@users.noreply.github.com>
2022-09-17 14:52:19 +0000

    update debian11-arm32.sh

--- assets/debian11-arm32.sh
+++ assets/debian11-arm32.sh
@@ -4,10 +4,10 @@
 DISTRO_NAME="Debian ARMv7"
 DISTRO_COMMENT="32 Bit version of Modified Debian Stable (works with ARM64 too)"
 
-TARBALL_URL['aarch64']="https://github.com/SUFandom/debdroid/releases/download/d-1.4.3/debian-arm32.tar.xz"
-TARBALL_SHA256['aarch64']="a3b78795cf83c024d2335aa9ec29b5de73a421020961dc15e88ae4a37e35e481"
-TARBALL_URL['arm']="https://github.com/SUFandom/debdroid/releases/download/d-1.4.3/debian-arm32.tar.xz"
-TARBALL_SHA256['arm']="a3b78795cf83c024d2335aa9ec29b5de73a421020961dc15e88ae4a37e35e481"
+TARBALL_URL['aarch64']="https://github.com/SUFandom/debdroid/releases/download/d-1.5/debian-arm32.tar.xz"
+TARBALL_SHA256['aarch64']="l4d907f0b596b5040fbf0fa41c9da5eea9049ff64bf2f54ddbd3ab0e317b16aa9"
+TARBALL_URL['arm']="https://github.com/SUFandom/debdroid/releases/download/d-1.5/debian-arm32.tar.xz"
+TARBALL_SHA256['arm']="l4d907f0b596b5040fbf0fa41c9da5eea9049ff64bf2f54ddbd3ab0e317b16aa9"
 
 
 distro_setup () {
